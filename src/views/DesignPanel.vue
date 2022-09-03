be422a2892aefe5bff66e39cd622937a56f7df73
Kibiwot <kibiwotvincent@gmail.com>
2022-09-03 21:54:41 +0300

    refactor: add draggable item configs

--- src/views/DesignPanel.vue
+++ src/views/DesignPanel.vue
@@ -20,9 +20,20 @@ import Navbar from '@/components/common/Navbar.vue'
 			<button class="btn btn-light fdpf-element" data-fpdf="settextcolor" data-is-new-element="true">Text color</button>
 		</div>
 		<div class="panel">
-			<div class="A4 parent">
-				<vue-draggable-resizable :parent=true :w=700 :h=30>
-					<p>You can drag me around and resize me as you wish.</p>
+			<div class="A4">
+				<vue-draggable-resizable v-for="(draggable, index) in this.draggables"
+				:key=index
+				:parent=true 
+				:x=draggable.left 
+				:y=draggable.top 
+				:w=draggable.width 
+				:h=draggable.height
+				@activated="onActivated(index)" 
+				@deactivated="onDeactivated"
+				@resizing="onResize"
+				@dragging="onDrag"
+				>
+					<i style="font-size: 11px;">{{draggable.text}}</i>
 				</vue-draggable-resizable>
 			</div>
 		</div>
@@ -31,7 +42,128 @@ import Navbar from '@/components/common/Navbar.vue'
 			<button class="btn btn-danger fdpf-element" id="clear_fpdf">Clear</button>
 			<button class="btn btn-warning fdpf-element" id="save_design">Save</button>
 			<button class="btn btn-primary fdpf-element float-right ml-1" id="show_output">Show Output</button>
-			<h1>dfd</h1>
+			<div class="draggable-meta">
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Height</label>
+					</div>
+					<div class="right">
+						<input type="text" :value=this.activeDraggable.height />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Width</label>
+					</div>
+					<div class="right">
+						<input type="text" :value=this.activeDraggable.width />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Top</label>
+					</div>
+					<div class="right">
+						<input type="text" :value=this.activeDraggable.top />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Left</label>
+					</div>
+					<div class="right">
+						<input type="text" :value=this.activeDraggable.left />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Text</label>
+					</div>
+					<div class="right">
+						<input type="text" :value=this.activeDraggable.text />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Font Family</label>
+					</div>
+					<div class="right">
+						<input type="text" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Font Style</label>
+					</div>
+					<div class="right">
+						<input type="text" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Font Size</label>
+					</div>
+					<div class="right">
+						<input type="text" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Font Color</label>
+					</div>
+					<div class="right">
+						<input type="color" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Border</label>
+					</div>
+					<div class="right">
+						<input type="text" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Border Size</label>
+					</div>
+					<div class="right">
+						<input type="text" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Border Color</label>
+					</div>
+					<div class="right">
+						<input type="color" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Background</label>
+					</div>
+					<div class="right">
+						<input type="text" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Background Color</label>
+					</div>
+					<div class="right">
+						<input type="color" />
+					</div>
+				</div>
+				<div class="draggable-meta-config">
+					<div class="left">
+						<label>Text Align</label>
+					</div>
+					<div class="right">
+						<input type="text" />
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 </template>
@@ -43,26 +175,55 @@ import Navbar from '@/components/common/Navbar.vue'
 		components: {
 			VueDraggableResizable
 		},
-		data: function () {
+		data() {
 			return {
-				width: 0,
-				height: 0,
-				x: 0,
-				y: 0
+				draggables: [],
+				activeDraggable: {}
 			}
 		},
+		mounted() {
+			this.initDraggables()
+		},
 		methods: {
-			onResize: function (x, y, width, height) {
-				this.x = x
-				this.y = y
-				this.width = width
-				this.height = height
+			initDraggables() {
+				this.draggables.push({
+					height: 30,
+					width: 200,
+					top: 20,
+					left: 20,
+					text: 'Draggable Content',
+				});
+				
+				this.draggables.push({
+					height: 30,
+					width: 300,
+					top: 40,
+					left: 20,
+					text: 'Draggable Content Two',
+				});
 			},
-			onDrag: function (x, y) {
-				this.x = x
-				this.y = y
+			onResize: function (x) {
+				console.log(x.left);
+				this.activeDraggable.left = x.left
+				this.activeDraggable.top = x.top
+				this.activeDraggable.width = x.width
+				this.activeDraggable.height = x.height
+			},
+			onDrag: function (x) {
+				this.activeDraggable.left = x.left
+				this.activeDraggable.top = x.top
+			},
+			onActivated (index) {
+				this.activeDraggable = this.draggables[index];
+			},
+			onDeactivated () {
+				this.active = false
 			}
+		},
+		computed: {
+			
 		}
+		
 	};
 </script>
 
@@ -81,9 +242,6 @@ import Navbar from '@/components/common/Navbar.vue'
 	box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
 	color: #000;
 }
-.parent {
-  
-}
 button {
 	color: rgb(236, 72, 153);
 	border: 1px solid #d3d3d3;
@@ -93,5 +251,36 @@ button {
 	padding: 10px; 
 	margin-right: 30px; 
 	width: 400px;
+	background: #fff;
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+	color: #000;
+	border-radius: 4px;
 }
+.right-panel .draggable-meta {
+	background: #f3f4f6;
+	margin-top: 20px;
+	padding: 10px;
+}
+.right-panel .draggable-meta .draggable-meta-config {
+	padding: 8px 0;
+	border-bottom: 1px solid #d3d3d3;
+}
+.right-panel .draggable-meta .draggable-meta-config .left, .right-panel .draggable-meta .draggable-meta-config .right{
+	width: 50%;
+	display: inline-block;
+}
+.right-panel .draggable-meta .draggable-meta-config label {
+	margin-right: 20px;
+}
+.right-panel .draggable-meta .draggable-meta-config input {
+	width: 100%;
+	border: 1px solid #d3d3d3;
+	border-radius: 4px;
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+	padding: 0 4px;
+}
+.right-panel .draggable-meta .draggable-meta-config input:focus {
+	outline: none;
+}
+
 </style>
