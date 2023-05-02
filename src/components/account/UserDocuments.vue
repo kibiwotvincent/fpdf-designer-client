da3aabea06180fd33cf29a834b0afe5d9807ccf7
Kibiwot <kibiwotvincent@gmail.com>
2023-05-02 21:05:19 +0300

    refactor: change how design panel is initialized from saved documents

--- src/components/account/UserDocuments.vue
+++ src/components/account/UserDocuments.vue
@@ -3,38 +3,45 @@
 </script>
 
 <template>
-    <div class="flex justify-between">
-		<h2 class="w-full text-2xl font-bold text-gray-800">
-			Saved documents
-		</h2>
-	</div>
 	<div class="flex justify-center mt-8" v-show="isLoading">
 	<Spinner :size=6 color="red-400" text="Loading saved documents..." :show-text=true />
 	</div>
 	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center mt-4">
-		<div class="w-full" v-for="document in documents" :key="document.uuid">
+		<div class="w-full" v-for="document in documents" :key="document.id">
 			<div
 			class="block rounded-lg shadow-lg">
-			<router-link :to="'/design-panel/'+document.uuid">
 				<img
-				class="rounded-lg w-full transform transition hover:scale-105 duration-300 ease-in-out"
-				src="http://localhost:8080/1.PNG"
-				:alt="document.uuid" />
-			</router-link>
+				class="rounded-lg w-full cursor-pointer transform transition hover:scale-105 duration-300 ease-in-out"
+				:src=document.thumbnail
+				:alt="document.name"
+				@click="initFromSource('documents', document.id)"
+				/>
 			</div>
 		</div>
 	</div>
+	<button class="hidden" ref="launchOverlayModal"
+	data-te-toggle="modal"
+	data-te-target="#overlayModal"
+	data-te-ripple-init
+	data-te-ripple-color="light"
+	></button>
+	<overlay-modal text="Initializing design panel..." :dismiss=dismissOverlayModal />
 </template>
 
 <script>
 	import createHttp from '@/axios.js'
+	import OverlayModal from '@/components/form/OverlayModal.vue'
 	
 	export default {
 		name: 'DocumentTemplates',
+		components: {
+			OverlayModal
+		},
 		data() {
 			return {
 				documents: [],
-				isLoading: true
+				isLoading: true,
+				dismissOverlayModal : false
 			}
 		},
 		mounted() {
@@ -44,6 +51,28 @@
 					this.isLoading = false
 					this.documents = response.data
 				})
+		},
+		methods: {
+			initFromSource(source, id) {
+				const url = process.env.VUE_APP_API_URL+'/api/workspace/init/'+source+'/'+id
+				this.initDesignPanel(url)
+			},
+			async initDesignPanel(url) {
+				this.$refs.launchOverlayModal.click()
+				const http = createHttp()
+				http.get(url)
+				.then((response) => {
+					//save setup & defaults data to be used later by design panel
+					localStorage.setItem('setup', JSON.stringify(response.data.setup))
+					localStorage.setItem('defaults', JSON.stringify(response.data.defaults))
+					
+					//dismiss overlay modal
+					this.dismissOverlayModal = true
+					
+					//redirect to design panel
+					this.$router.push('/design-panel/'+response.data.id)
+				})
+			}
 		}
 	};
 </script>
--- src/views/account/UserDocuments.vue
+++ src/views/account/UserDocuments.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navbar from '@/components/account/Navbar.vue'
 import UserDocuments from '@/components/account/UserDocuments.vue'
+import { PlusIcon } from '@heroicons/vue/20/solid'
 </script>
 
 <template>
@@ -8,6 +9,17 @@ import UserDocuments from '@/components/account/UserDocuments.vue'
 	<section class="bg-blue-50 py-8">
 		<div class="container mx-auto pb-12 text-gray-800">
 			<div class="bg-blue-50 py-8 px-4 mt-8">
+				<div class="flex justify-between mb-4">
+					<div class="inline-block pt-4">
+						Saved Documents
+					</div>
+					<router-link to="/document/choose-template">
+					<button class="bg-red-400 hover:underline text-white rounded py-2 px-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+						<PlusIcon class="inline-block h-5 w-5 mb-1"/>
+						Create New Document
+					</button>
+					</router-link>
+				</div>
 				<UserDocuments />
 			</div>
 		</div>
